d844a7031ce6d0891e34b4048efaf55b865612fd
MDBoticano <MDBoticano@users.noreply.github.com>
2019-08-07 10:23:37 -0700

    More form styling, still not satisfied

--- src/AddExercise.js
+++ src/AddExercise.js
@@ -5,35 +5,41 @@ const AddExercise = ({ submitExercise, nameHandler, formName,
   categoriesHandler, formCategories }) => {
 
   const formVisibleText = 'X'
-  const formInvisibleText = 'Add Exercise'
+  const formInvisibleText = '+'
   
   const [formIsVisible, setFormIsVisible] = useState(false)
   const [formToggleText, setFormToggleText] = useState(formInvisibleText)
 
   const toggleForm = () => {
     if (!formIsVisible) {
+      document.getElementById('AddExercise').classList.remove('formIsClosed')
+      document.getElementById('AddExercise').classList.add('formIsOpen')
       document.getElementById('newExerciseForm').classList.remove('hidden')
+      document.getElementById('addExerciseToggle').classList.remove('openForm')
       document.getElementById('addExerciseToggle').classList.add('closeForm')
       setFormIsVisible(true)
       setFormToggleText(formVisibleText)
     } else if (formIsVisible) {
+      document.getElementById('AddExercise').classList.remove('formIsOpen')
+      document.getElementById('AddExercise').classList.add('formIsClosed')
       document.getElementById('newExerciseForm').classList.add('hidden')
       document.getElementById('addExerciseToggle').classList.remove('closeForm')
+      document.getElementById('addExerciseToggle').classList.add('openForm')
       setFormIsVisible(false)
       setFormToggleText(formInvisibleText)
     }
   }
 
   return (
-    <div id="AddExercise">
-      <button id="addExerciseToggle" onClick={toggleForm}>
+    <div id="AddExercise" className="formIsClosed">
+      <button id="addExerciseToggle" className="openForm" onClick={toggleForm}>
         {formToggleText}
       </button>
       <form id="newExerciseForm" className="hidden" onSubmit={submitExercise}>
         <div id="formName" className="formField">
           <label htmlFor="name">name</label>
           <input id="formNameInput"
-            type="text"  name="exercise_name" 
+            type="text" name="exercise_name" 
             onChange={nameHandler} value={formName}
           />
         </div>
