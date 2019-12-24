b576dbe1da8971b1c9f25d5596f5cf76ff693b5c
mdboticano <mdboticano@gmail.com>
2019-12-23 16:36:03 -0800

    Use fake JSON data for exercises
    The data comes from wikipedia:
    https://en.wikipedia.org/wiki/List_of_weight_training_exercises

---
+++ src/data/DummyExercises.js
@@ -0,0 +1,49 @@
+const DummyExercises = [
+  {
+    "id": 1,
+    "name": "Squat",
+    "description" : "A squat is a strength exercise in which the trainee lowers their hips from a standing position and then stands back up.",
+    "tags" : [
+      "Quads", "Glutes", "Abs"
+    ],
+  }, 
+  {
+    "id": 2,
+    "name": "Deadlift",
+    "description" : "The deadlift is a weight training exercise in which a loaded barbell or bar is lifted off the ground to the level of the hips, torso perpendicular to the floor, before being placed back on the ground.",
+    "tags" : [
+      "Quads", "Hamstrings", "Glutes", "Lower Back"
+    ],
+  }, 
+  {
+    "id": 3,
+    "name": "Benchpress",
+    "description" : "The bench press is an upper-body weight training exercise in which the trainee presses a weight upwards while lying on a weight training bench.",
+    "tags" : [
+      "Pecs", "Triceps"
+    ],
+  }, {
+    "id": 4,
+    "name": "Bicep Curl",
+    "description" : "A common factor of each curl is a 'curling' motion, where a weight (attached to, or used in conjunction with, an item of equipment listed above) is lifted up until the forearms are vertical[1] with the elbows and upper arm remaining close to the body.",
+    "tags" : [
+      "Biceps"
+    ],
+  }, {
+    "id": 5,
+    "name": "Shoulder Shrug",
+    "description" : "The lifter stands erect, hands about shoulder width apart, and raises the shoulders as high as possible, and then lowers them, while not bending the elbows, or moving the body at all. ",
+    "tags" : [
+      "Traps"
+    ],
+  }, {
+    "id": 6,
+    "name": "Crunch",
+    "description" : "An approved crunch begins with you lying down, one knee bent, and hands positioned beneath your lower back for support. Gently lift your head and shoulders, hold briefly and relax back down.",
+    "tags" : [
+      "Traps"
+    ],
+  }, 
+];
+
+export default DummyExercises;
