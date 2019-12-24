b968b136c607aa03649e2b9eae4bc42b38532141
mdboticano <mdboticano@gmail.com>
2019-12-23 18:45:55 -0800

    Create TagsRow styled-component
    - Improve Cards, Navigation, and Tags components

--- src/App.jsx
+++ src/App.jsx
@@ -3,31 +3,25 @@ import React from 'react';
 import Navigation from './components/Navigation';
 import Exercises from './components/Exercises';
 import Card from './components/Card';
-import Tag from './components/Tag';
+import TagsRow from './components/TagsRow';
 
 import DummyExercises from './data/DummyExercises';
 
-const listTags = (tags) => {
-  const allTags = tags.map(tag => <Tag text={tag} key={tag} />);
-  return (
-    <div className="tags-row">
-      {allTags}
-    </div>
-  );
-}
+
+
 
 const listExercises = (exercises) => {
   const cards = exercises.map(exercise => {
     const { id, name, description, tags } = exercise;
 
     return (
-      <Card title={name} description={description} key={id}>
-        {listTags(tags)}
+      <Card className="Card" title={name} description={description} key={id}>
+        <TagsRow tags={tags} />
       </Card>
     );
   });
   return <>{cards}</>
-}
+};
 
 const App = () => {
   return (
--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,14 +1,6 @@
 import React from 'react';
 import styled from 'styled-components';
 
-const StyledCardTitle = styled.p`
-  margin: 0;
-
-  font-family: 'SF Pro Display';
-  font-size: 1.5rem;
-  line-height: 1.75rem;
-`
-
 const StyledCard = styled.div`
   margin: 0.5rem 0;
   padding: 1rem;
@@ -20,10 +12,17 @@ const StyledCard = styled.div`
   &:hover {
     box-shadow: 1px 1px 2px rgba(0, 0, 0, 0.25);
   }
+`;
 
-`
+const CardTitle = styled.p`
+  margin: 0;
 
-const StyledCardDescription = styled.p`
+  font-family: 'SF Pro Display';
+  font-size: 1.5rem;
+  line-height: 1.75rem;
+`;
+
+const CardDescription = styled.p`
   margin: 0;
   margin-top: 5px;
 
@@ -32,38 +31,21 @@ const StyledCardDescription = styled.p`
   text-overflow: ellipsis;
 
   font-family: 'SF Pro Text';
+  line-height: 1.25rem;
   font-size: 1rem;
-  line-height: 1.125rem;
+
 
   color: gray;
-`
-
-const CardTitle = (props) => {
-  return (
-    <StyledCardTitle attribute={props.style}>
-      {props.title}
-    </StyledCardTitle>
-  );
-}
-
-const CardDescription = (props) => {
-  return (
-    <StyledCardDescription attribute={props.style}>
-      {props.description}
-    </StyledCardDescription>
-  )
-}
+`;
 
 const Card = (props) => {
   return (
-    <StyledCard attribute={props.style}>
-      <CardTitle title={props.title} />
-      <CardDescription description={props.description} />
+    <StyledCard>
+      <CardTitle>{props.title}</CardTitle>
+      <CardDescription>{props.description}</CardDescription>
       {props.children}
     </StyledCard>
   );
-}
-
-
+};
 
 export default Card;
