727acc8f6b9acfdcb73bea6642fe65965c07ffbf
Marlo Boticano <m.boticano@gmail.com>
2019-12-27 14:26:16 -0800

    Use useContext hook to access ThemeContext

--- src/components/Exercises.jsx
+++ src/components/Exercises.jsx
@@ -1,9 +1,52 @@
-import React, { useState } from 'react';
+import React, { useState, useContext } from 'react';
 
 import ExercisesSearch from './ExercisesSearch';
 import Card from './Card';
 
 import DummyExercises from '../data/DummyExercises';
+
+import styled, { ThemeContext } from 'styled-components';
+
+const StyledExercises = styled.div`
+  max-height: 100vh;
+
+  margin: 0;
+  padding: 0;
+
+  display: flex;
+  flex-direction: column;
+`
+
+const StyledHeader = styled.header`
+  margin: 0;
+  padding-top: 1.5rem;
+
+  background-color: white;
+
+  box-shadow: 0 2px 4px rgba(0, 0, 0, 0.5);
+
+  /* so cards are below the header despite sibling */
+  z-index: 100; 
+
+  .page-title {
+    margin: 0;
+    padding: 0;
+
+    padding-left: 1rem;
+
+    font-family: ${props => props.theme.fonts.standard};
+    font-size: ${props => props.theme.fontSize.xxl};
+    line-height: 2.5rem;
+  }
+`
+
+const StyledList = styled.div`
+  margin: 0;
+  margin-bottom: 4.5rem;
+  padding: 0 0.5rem;
+
+  overflow: auto;
+`
 
 const Exercises = () => {
   const exercises = DummyExercises;
@@ -13,15 +56,18 @@ const Exercises = () => {
     <Card className="Card" exercise={exercise} key={exercise.id} />
   ));
 
+  const theme = useContext(ThemeContext);
+
   return (
-    <div className="Exercises">
-      <header>
+    <StyledExercises theme={theme}>
+      <StyledHeader>
+        <p className="page-title">Exercises</p>
         <ExercisesSearch data={exercises} setActiveData={setActiveExercises} />
-      </header>
-      <div className="ExercisesList">
+      </StyledHeader>
+      <StyledList className="ExercisesList">
         {cardsList}
-      </div>
-    </div>
+      </StyledList>
+    </StyledExercises>
   );
 }
 
--- src/components/ExercisesSearch.jsx
+++ src/components/ExercisesSearch.jsx
@@ -1,17 +1,43 @@
-import React, { useState } from 'react';
-
-import styled from 'styled-components';
+import React, { useState, useContext } from 'react';
 
 import debounce from 'lodash.debounce'; //implement yourself later
 
 import { filterData } from '../utilities/search-utilities';
 
+import styled, { ThemeContext } from 'styled-components';
+
 
 const StyledSearch = styled.div`
   margin: 0;
-  padding: 2rem 1rem 1rem;
+  padding: .75rem 1rem;
 
   background-color: white;
+
+  input {
+    width: 100%;
+    height: 2rem;
+    margin: 0;
+    padding: 0.25rem 0.5rem;
+
+    box-sizing: border-box;
+
+    border-radius: 0.75rem;
+
+    color: hsl(0, 0%, 59%);
+
+    background-color: hsl(0, 0%, 90%);
+
+    font-family: ${props => props.theme.fonts.standard};
+    font-size: ${props => props.theme.fontSize.m};
+    line-height: 1.25rem;
+
+    :focus {
+      outline: none;
+
+      color: hsl(0, 0%, 20%);
+      background-color: white;
+    }
+  }
 `;
 
 // Debounce delay in miliseconds
@@ -38,13 +64,15 @@ const ExercisesSearch = ({ data, setActiveData }) => {
     debouncedFilter(data, queryValue, queryables, setActiveData);
   }
 
+  const theme = useContext(ThemeContext);
+
   return (
-    <StyledSearch className="ExercisesSearch">
+    <StyledSearch className="ExercisesSearch" theme={theme}>
        <input
         type="text"
         value={query}
-        placeholder={"search"}
-        onChange={(e) => handleChange(e)}
+        placeholder={"Search"}
+        onChange={(event) => handleChange(event)}
       />
     </StyledSearch>
   );
