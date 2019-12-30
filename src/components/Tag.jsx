a5c11345551b3d9d5850fe93d571d7c8b0df20a0
Marlo Boticano <m.boticano@gmail.com>
2019-12-30 10:26:39 -0800

    Style Tag component

--- src/components/Tag.jsx
+++ src/components/Tag.jsx
@@ -3,19 +3,27 @@ import styled, { ThemeContext } from 'styled-components';
 import SearchContext from './SearchContext';
 
 const Button = styled.button`
-  border: solid 1px hsl(0, 0%, 59%);
   padding: 0.25rem 0.5rem;
 
+  border: solid 1px hsl(0, 0%, 59%);
+
   border-radius: 0.25rem;
-
-  background-color: white;
-
+  
   font-size: ${props => props.theme.fontSize.s};
   line-height: 1.25rem;
+
   color: hsl(0, 0%, 59%);
+  background-color: white;
+  outline: none;
 
   &:not(:last-child) {
     margin-right: 0.25rem;
+  }
+
+  :hover {
+    border: solid 1px hsl(0, 0%, 39%);
+    color: white;
+    background-color: hsl(0, 0%, 39%);
   }
 `;
 
